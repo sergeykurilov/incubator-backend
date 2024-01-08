076e4210a12888b51942bef935866a56d02673be
sergeykurilov <kurilovsergey15@gmail.com>
2024-01-08 13:33:40 +0200

    initial

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port: string | number = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/api", router);
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
