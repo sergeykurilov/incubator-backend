864d075940063783961d05151a645eb63dccdb74
sergeykurilov <kurilovsergey15@gmail.com>
2024-07-16 03:24:56 +0300

    fix: vercel

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -3,13 +3,16 @@ import { config, DotenvConfigOutput, DotenvParseOutput } from "dotenv";
 import { inject, injectable } from "inversify";
 import { SERVICE_IDENTIFIER } from "../modules/common/consts/service-identifiers.consts";
 import { ILogger } from "../modules/common/logger/logger.interface";
+import path from "path";
 
 @injectable()
 export class ConfigService implements IConfigService {
   private config: DotenvParseOutput;
 
   constructor(@inject(SERVICE_IDENTIFIER.ILogger) private logger: ILogger) {
-    const result: DotenvConfigOutput = config();
+    const result: DotenvConfigOutput = config({
+      path: path.resolve(__dirname, ".env"),
+    });
     if (result.error) {
       this.logger.error(
         "[ConfigService] Cannot read file .env or it doesnt exist",
